f9c094b6a3151bf001e67e0e5f876050fe00f902
shawnh29 <Shawnhan20@gmail.com>
2023-10-27 00:45:57 -0700

    new scene for q1, grayscale done

--- src/main/java/com/example/cmpt365_tiff_reader/HelloApplication.java
+++ src/main/java/com/example/cmpt365_tiff_reader/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.cmpt365_tiff_reader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -20,4 +21,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/com/example/cmpt365_tiff_reader/MainController.java
+++ src/main/java/com/example/cmpt365_tiff_reader/MainController.java
@@ -1,6 +1,9 @@
 package com.example.cmpt365_tiff_reader;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.*;
@@ -16,6 +19,9 @@ import java.nio.IntBuffer;
 
 public class MainController {
 
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
     @FXML
     private ImageView image;
     @FXML
@@ -24,6 +30,12 @@ public class MainController {
     private Label width;
     @FXML
     private Label height;
+    @FXML
+    private Button goToGrayScaleButton;
+    @FXML
+    private ImageView originalImage;
+    @FXML
+    private ImageView grayscaleImage;
     @FXML
     public void openFileClicked() throws IOException {
         FileChooser fc = new FileChooser();
@@ -48,11 +60,75 @@ public class MainController {
             height.setText("Height: " + buffImage.getHeight());
             width.setVisible(true);
             height.setVisible(true);
+            goToGrayScaleButton.setVisible(true);
         }
     }
     @FXML
     public void exitButtonClicked() {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
+    }
+    @FXML
+    public void grayScaleButtonClicked() throws IOException {
+        root = FXMLLoader.load(getClass().getResource("grayScaleScreen.fxml"));
+        stage = (Stage) goToGrayScaleButton.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Grayscale");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void makeGreyscale(File file) throws IOException {
+        BufferedImage buffImg = ImageIO.read(file);
+        int width = buffImg.getWidth();
+        int height = buffImg.getHeight();
+
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                int pixel = buffImg.getRGB(x,y);
+
+                int aVal = (pixel >> 24) & 0xff;
+                int rVal = (pixel >> 16) & 0xff;
+                int gVal = (pixel >> 8) & 0xff;
+                int bVal = pixel & 0xff;
+
+                int averageVal = (rVal + gVal + bVal) / 3;
+
+                pixel = (aVal << 24) | (averageVal << 16) | (averageVal << 8) | averageVal;
+                buffImg.setRGB(x,y,pixel);
+            }
+        }
+        BufferedImage newImg = new BufferedImage(buffImg.getWidth(), buffImg.getHeight(), BufferedImage.TYPE_INT_ARGB_PRE);
+        newImg.createGraphics().drawImage(buffImg, 0, 0, buffImg.getWidth(), buffImg.getHeight(), null);
+
+        int[] argb_arr = ((DataBufferInt) newImg.getRaster().getDataBuffer()).getData();
+        IntBuffer buff = IntBuffer.wrap(argb_arr);
+
+        PixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbPreInstance();
+        PixelBuffer<IntBuffer> pixelBuf = new PixelBuffer<>(newImg.getWidth(), newImg.getHeight(), buff, pixelFormat);
+
+        grayscaleImage.setImage(new WritableImage(pixelBuf));
+    }
+    @FXML
+    public void grayscaleFileButton() throws IOException {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter(".tif files", "*.tif"));
+        File file = fc.showOpenDialog(null);
+
+        if (file != null) {
+            BufferedImage buffImage = ImageIO.read(file);
+            BufferedImage newImg = new BufferedImage(buffImage.getWidth(), buffImage.getHeight(), BufferedImage.TYPE_INT_ARGB_PRE);
+            newImg.createGraphics().drawImage(buffImage, 0, 0, buffImage.getWidth(), buffImage.getHeight(), null);
+
+            int[] argb_arr = ((DataBufferInt) newImg.getRaster().getDataBuffer()).getData();
+            IntBuffer buff = IntBuffer.wrap(argb_arr);
+
+            PixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbPreInstance();
+            PixelBuffer<IntBuffer> pixelBuf = new PixelBuffer<>(newImg.getWidth(), newImg.getHeight(), buff, pixelFormat);
+
+            originalImage.setImage(new WritableImage(pixelBuf));
+            makeGreyscale(file);
+
+        }
     }
 }
