8194a193a32030c20b1948aa719075d5bd0ab0f7
shawnh29 <81540655+shawnh29@users.noreply.github.com>
2023-10-27 12:21:00 -0700

    getting started on q2, brightness

--- src/main/java/com/example/cmpt365_tiff_reader/MainController.java
+++ src/main/java/com/example/cmpt365_tiff_reader/MainController.java
@@ -27,15 +27,20 @@ public class MainController {
     @FXML
     private Button exitButton;
     @FXML
+    private Button brightnessButton;
+    @FXML
     private Label width;
     @FXML
     private Label height;
+    @FXML
+    private Label newImageLabel;
     @FXML
     private Button goToGrayScaleButton;
     @FXML
     private ImageView originalImage;
     @FXML
     private ImageView grayscaleImage;
+    private File file;
     @FXML
     public void openFileClicked() throws IOException {
         FileChooser fc = new FileChooser();
@@ -109,7 +114,12 @@ public class MainController {
             }
         }
         grayscaleImage.setImage(new WritableImage(makeViewableImg(buffImg)));
+        brightnessButton.setVisible(true);
     }
+
+//    public void lowerBrightness () {
+//        BufferedImage buffImage = ImageIO.read()
+//    }
     @FXML
     public void grayscaleFileButton() throws IOException {
         FileChooser fc = new FileChooser();
@@ -120,8 +130,35 @@ public class MainController {
             BufferedImage buffImage = ImageIO.read(file);
 
             originalImage.setImage(new WritableImage(makeViewableImg(buffImage)));
+            this.file = file;
             makeGreyscale(file);
 
+        }
+    }
+    @FXML
+    public void brightnessButtonClicked() throws IOException {
+        newImageLabel.setText("Reduced brightness by 50%");
+        BufferedImage bufImage = ImageIO.read(file);
+        int width = bufImage.getWidth();
+        int height = bufImage.getHeight();
+
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                int pixel = bufImage.getRGB(x,y);
+
+                int aVal = (pixel >> 24) & 0xff;
+                int rVal = (pixel >> 16) & 0xff;
+                int gVal = (pixel >> 8) & 0xff;
+                int bVal = pixel & 0xff;
+
+                aVal *= 0.5;
+                rVal *= 0.5;
+                gVal *= 0.5;
+                bVal *= 0.5;
+
+                pixel = (aVal << 24) | (rVal << 16) | (gVal << 8) | bVal;
+                bufImage.setRGB(x,y,pixel);
+            }
         }
     }
 }
